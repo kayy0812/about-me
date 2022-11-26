a6dbdcfcbf380f900445aea65e630c14543423e8
kayy0812 <kayy0812.levanloc@gmail.com>
2022-11-26 15:28:24 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,14 @@ const startBtn = document.querySelector(".start");
 const firstCircle = document.querySelector(".panel-circle");
 const clock = document.querySelector(".digital-clock");
 const startButton = document.querySelector(".start");
+const blurBg = document.querySelector(".blur-bg");
 const preload = document.querySelector(".preload");
 
 startBtn.addEventListener("click", (e) => {
     firstCircle.classList.add("panel-circle-first--on-btn-hover");
     clock.classList.add("digital-clock--fade-out");
     startButton.classList.add("start--fade-out");
+    blurBg.classList.add("blur-bg--fade-out");
     setTimeout(() => preload.style.display = "none", 2000);
     clearInterval(time);
 });
